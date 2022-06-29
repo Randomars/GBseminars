b0d11f1f0a787645ae0dcbd48472f975962baa8f
Arseny <golem9@mail.ru>
2022-06-29 20:09:25 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -14,9 +14,42 @@ if (day==7) Console.WriteLine("Воскресенье");
 if ((day>7) || (day<1)) Console.WriteLine("Такого дня в неделе нет");
 */
 
+/*
 // Задача 2. Вывести квадрат числа
 
 Console.Write("Введите число:");
 int num=Convert.ToInt32(Console.ReadLine());
 Console.Write("Квадрат числа равен " + num*num);
+*/
 
+// Задача 3. Наименьшее и наибольшее из 2х чисел
+
+int min, max;
+
+Console.Write("Введите 1е число:");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 1е число:");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2) 
+{
+    max=num1;
+    min=num2;
+}
+else
+{
+    if (num2>num1) 
+    {
+        max=num2;
+        min=num1;
+    }
+    else
+    {
+        Console.WriteLine("Числа равны");
+        max=num1;
+        min=num2;
+    }
+}
+
+Console.WriteLine("Большее число " + max);
+Console.WriteLine("Меньшее число " + min);
