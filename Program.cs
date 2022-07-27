a10b05c8195384425ff2012d741ad1ceb0109a7e
Arseny <golem9@mail.ru>
2022-07-27 20:46:30 +0300

    Task 43

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ int InputNum(string tip)
 
 // Задача 41: Пользователь вводит M чисел. Cколько чисел больше 0 ввёл пользователь.
 
-
+/*
 int[] InputArray(int size)
 {
     int[] newArray = new int[size];
@@ -42,122 +42,27 @@ int[] myArray = InputArray(m);
 ShowArray(myArray);
 CountPositive(myArray);
 
-// Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-/*
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-    int[] newArray = new int[size];
-
-    for (int i = 0; i < size; i++)
-        newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
-}
-
-
-
-int GetEvenCount(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-        if (array[i] % 2 == 0) count++;
-    return count;   
-}
-
-int arraySize = 20;
-int arrayMinValue = 100;
-int arrayMaxValue = 1000;
-
-int[] myArray = CreateRandomArray(arraySize, arrayMinValue, arrayMaxValue);
-Console.WriteLine("Ваш массив: ");
-ShowArray(myArray);
-Console.WriteLine("Количество четных чисел в массиве: " + GetEvenCount(myArray));
-
 */
 
-// Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-/*
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+// Задача 43: Найти точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем. b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void LineCross(int k1, int b1, int k2, int b2)
 {
-    int[] newArray = new int[size];
-
-    for (int i = 0; i < size; i++)
-        newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
-}
-
-void ShowArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-
-int GetOddIndexSumm(int[] array)
-{
-    int summ = 0;
-    for (int i = 0; i < array.Length; i++)
-        if (i % 2 > 0) summ += array[i];
-    return summ;
-}
-
-int arraySize = 10;
-int arrayMinValue = -99;
-int arrayMaxValue = 100;
-
-int[] myArray = CreateRandomArray(arraySize, arrayMinValue, arrayMaxValue);
-Console.WriteLine("Ваш массив: ");
-ShowArray(myArray);
-Console.WriteLine("Сумма нечетных позиций: " + GetOddIndexSumm(myArray));
-
-*/
-
-
-
-// Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-
-/*
-double[] CreateArray(int size)
-{
-    double[] newArray = new double[size];
-
-    Console.WriteLine($"Creating array {size}-size.");
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write($"Input {i + 1} element:");
-        newArray[i] = Convert.ToDouble(Console.ReadLine());
-    }
-    Console.WriteLine();
-    return newArray;
-}
-
-void ShowArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-
-double FindMax(double[] array)
-{
-    double res = array[0];
-    for (int i = 1; i < array.Length; i++)
-        if (array[i] > res) res = array[i];
-    return res;
-}
-
-double FindMin(double[] array)
-{
-    double res = array[0];
-    for (int i = 1; i < array.Length; i++)
-        if (array[i] < res) res = array[i];
-    return res;
+    double x = b2 - b1;
+    double x1 = k1 - k2;
+    x = x / x1;
+    
+    // double x = (b2-b1)/(k1-k2);
+    
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения ({x}; {y}).");
 }
 
 
-Console.Write("Введите размер массива: ");
-int mySize = Convert.ToInt32(Console.ReadLine());
-double[] myArray = CreateArray(mySize);
-Console.WriteLine("Ваш массив: ");
-ShowArray(myArray);
-Console.WriteLine("Разница между максимальным и минимальным: " + (FindMax(myArray) - FindMin(myArray)));
-*/
+Console.WriteLine("Задайте параметры двух прямых заданных уравнениями вида (y = k * x + b) для поиска точки пересечения.");
+int k1 = InputNum("Значение k1: ");
+int b1 = InputNum("Значение b1: ");
+int k2 = InputNum("Значение k2: ");
+int b2 = InputNum("Значение b2: ");
+LineCross(k1, b1, k2, b2);
